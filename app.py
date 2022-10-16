9e115bb0ee938e98cc2a57015e8870f2d836a33e
Erik Nathan <erik@eriknathan.localdomain>
2022-10-16 11:32:07 -0300

    subindo imagem do script rodando

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import requests
-import json
 import pandas as pd
+import json
+import requests
 # executar o <pip install -r requirements.txt> para instalar as dependencias
 
 data = requests.get('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
@@ -18,6 +18,6 @@ for dados in json_data['cand']:
         part.append(dados['cc'])
         porcent.append(dados['pvap'])
 
-eleicao = pd.DataFrame(list(zip(candt, votos, part, porcent)), columns = ['   CANDIDATO', '   Nº VOTOS', '   PARTIDO', 'PORCENTAGEM'])
+eleicao = pd.DataFrame(list(zip(candt, votos, part, porcent)), columns = ['   CANDIDATO', '   Nº VOTOS', '   PARTIDO', '   PORCENTAGEM'])
 
 print(eleicao)
